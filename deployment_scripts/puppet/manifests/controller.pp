95a1302a6c178b8a128a3e930213a57f2936cdf9
Moreno, Adrian <adrian@morenomartinez.com>
2015-09-08 16:37:43 +0200

    added manifest for handling scaleio roles

---
+++ deployment_scripts/puppet/manifests/controller.pp
@@ -0,0 +1,4 @@
+$fuel_settings = parseyaml($astute_settings_yaml)
+$nodes_hash = $::fuel_settings['nodes']
+$node = filter_nodes($nodes_hash,'name',$::hostname)
+$internal_address = $node[0]['internal_address']
